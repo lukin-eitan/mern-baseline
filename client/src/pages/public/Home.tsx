c4454fd208236c1651a8b81685f9b9c802b6a2fa
Eitan Lukin <eitanlukin@Eitans-MacBook-Pro-2.local>
2024-01-07 21:18:53 +0200

    added some images for readme

--- client/src/pages/public/Home.tsx
+++ client/src/pages/public/Home.tsx
@@ -1,14 +1,34 @@
+import { Stack, Typography } from '@mui/material';
 import { useConnectedUser } from '../../hooks/useConnectedUser';
+import mernBaselineLogo from '../../../../images/mern-baseline-logo.jpg';
 
 const Home = () => {
   const connectedUser = useConnectedUser();
   return (
-    <div>
-      {connectedUser?.isConnected
-        ? 'Hello ' + connectedUser.user?.firstName
-        : 'Hello Guest!'}
-      !
-    </div>
+    <Stack spacing={2} alignItems="center" justifyContent="center">
+      <img
+        srcSet={`${mernBaselineLogo}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+        src={`${mernBaselineLogo}?w=164&h=164&fit=crop&auto=format`}
+        alt="MERN Baseline"
+      />
+      <Typography
+        variant="h6"
+        noWrap
+        sx={{
+          mr: 2,
+          display: { xs: 'none', md: 'flex' },
+          fontFamily: 'monospace',
+          fontWeight: 700,
+          letterSpacing: '.3rem',
+          color: 'inherit',
+          textDecoration: 'none'
+        }}
+      >
+        {connectedUser?.isConnected
+          ? 'Hello ' + connectedUser.user?.firstName
+          : 'Hello Guest'}
+      </Typography>
+    </Stack>
   );
 };
 
